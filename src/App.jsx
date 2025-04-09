f991afa4db18b73254dd84b2e75c7b2af0537895
tefin bose <tefinbose2003@gmail.com>
2025-04-09 12:59:14 +0530

    Tefin

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Tefin from "./Components/Tefin";
+
+function App() {
+  return (
+    <>
+      <Tefin />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Tefin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tefin() {
+  return (
+    <div>Tefin</div>
+  )
+}
+
+export default Tefin
